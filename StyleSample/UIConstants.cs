dcf0f7f9eabf0176560b27fd783684ed025b286c
eddy <liguodong9063@126.com>
2020-04-02 11:11:04 +0800

    样式示例

---
+++ StyleSample/UIConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace StyleSample
+{
+    public static class UIConstants
+    {
+        /// <summary>
+        /// 标准控件高度
+        /// </summary>
+        public static double StandardControlHeight = 26;
+        /// <summary>
+        /// 日期控件默认宽度（需定宽的情况）
+        /// </summary>
+        public static double DateTimeControlWidth = 110;
+        /// <summary>
+        /// 标准字体大小
+        /// </summary>
+        public static double StandardFontSize = 14;
+    }
+}
